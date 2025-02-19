09be45c6457ec16ea7183495f8bb77e3ac299df5
DennisIrineu <dennispaulinoirineu@gmail.com>
2025-02-19 15:12:59 -0300

    Adicionando o nome personalizado

--- scm.c
+++ scm.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
 #include <time.h>
 
-void soma(int a, int b) {
+void soma(int a, int b)
+{
     printf("%d + %d = %d\n", a, b, a + b);
 }
 
-int main() {
+int main()
+{
+    char nome[50];
     time_t t;
     struct tm *tm_info;
 
     time(&t);
     tm_info = localtime(&t);
 
+    printf("Digite seu nome: ");
+    scanf("%s", nome);
+
     printf("Olá, Estudante!\n");
 
+    printf("Olá, %s!\n", nome);
+
     printf("%02d/%02d/%04d %02d:%02d:%02d\n",
-            tm_info->tm_mday,
-            tm_info->tm_mon + 1,
-            tm_info->tm_year + 1900,
-            tm_info->tm_hour,
-            tm_info->tm_min,
-            tm_info->tm_sec);
+           tm_info->tm_mday,
+           tm_info->tm_mon + 1,
+           tm_info->tm_year + 1900,
+           tm_info->tm_hour,
+           tm_info->tm_min,
+           tm_info->tm_sec);
 
     soma(5, 3);
     return 0;
