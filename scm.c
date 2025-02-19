995b047958e02e9b60930c75ae6652533014854b
DennisIrineu <dennispaulinoirineu@gmail.com>
2025-02-19 15:28:52 -0300

    Ajuste no printf

--- scm.c
+++ scm.c
@@ -6,28 +6,37 @@ void soma(int a, int b)
     printf("%d + %d = %d\n", a, b, a + b);
 }
 
-void multiplica(int a, int b) {
+void multiplica(int a, int b)
+{
     printf("%d x %d = %d\n", a, b, a * b);
 }
 
 // Nova função "subtrai" criada (por fmh@cin.ufpe.br em 17/02/2025 às 11h00)
-void subtrai(int a, int b) {
+void subtrai(int a, int b)
+{
     // Verifica se atende aos requisitos do sistema
-    if (b > a){
-    // Se não atende aos requisitos
+    if (b > a)
+    {
+        // Se não atende aos requisitos
         printf("Atencao: subtracao nao permitida!\n");
-    } else {
-    // Se atende aos requisitos
+    }
+    else
+    {
+        // Se atende aos requisitos
         printf("%d - %d = %d\n", a, b, a - b);
     }
 }
 
-void divide(int a, int b) {
-  if (b == 0) {
-    printf("Divisão por zero não permitido\n");
-  } else {
-    printf("%d / %d = %.2f\n", a, b, (float)a / b);
-  }
+void divide(int a, int b)
+{
+    if (b == 0)
+    {
+        printf("Divisão por zero não permitido\n");
+    }
+    else
+    {
+        printf("%d / %d = %.2f\n", a, b, (float)a / b);
+    }
 }
 
 int main()
@@ -46,9 +55,7 @@ int main()
     printf("Digite seu nome: ");
     scanf("%s", nome);
 
-    printf("Olá, Estudante!\n");
-
-    printf("Olá, %s!\n", nome);
+    printf("Olá, estudante %s!\n", nome);
 
     printf("%02d/%02d/%04d %02d:%02d:%02d\n",
            tm_info->tm_mday,
